f6826c2405d7287864814af22f5eebf64c3fbab0
tsk-nkgw <tasktas9@gmail.com>
2019-05-26 23:09:07 +0900

    imple main

--- src/main/java/trial/Main.java
+++ src/main/java/trial/Main.java
@@ -1,5 +1,6 @@
 package trial;
 
+import org.eclipse.jdt.core.dom.CompilationUnit;
 import trial.ast.ASTFactory;
 import trial.ast.construction.IASTConstructor;
 
@@ -13,5 +14,11 @@ public class Main {
         final Path directoryPath = Paths.get(args[0]);
         final List<Path> pathList = FileUtility.getAllJavaFilePath(directoryPath);
         final IASTConstructor astConstructor = ASTFactory.create(args[1]);
+        final List<CompilationUnit> unitList = astConstructor.construct(pathList);
+
+        for (final CompilationUnit unit : unitList) {
+            final MyVisitor visitor = new MyVisitor();
+            unit.accept(visitor);
+        }
     }
 }
